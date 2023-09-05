51aa783c1fc13d461f5081653c85dbcdd71c2cdb
Durvesh27 <132134129+Durvesh27@users.noreply.github.com>
2023-09-06 00:27:16 +0530

    updated

--- Frontend/my-app/src/Components/Api Config/index.js
+++ Frontend/my-app/src/Components/Api Config/index.js
@@ -3,13 +3,13 @@ import axios from "axios"
 const token=JSON.parse(localStorage.getItem("Token"))
 if(token){
 var api=axios.create({
-baseURL: "http://localhost:8000/api/v1",
+baseURL: "https://awdiz-practice-backend.onrender.com/api/v1",
 headers:{"Authorization":`Bearer${token}`}
 })
 }
 else{
     var api = axios.create({
-        baseURL: 'http://localhost:8000/api/v1'
+        baseURL: 'https://awdiz-practice-backend.onrender.com/api/v1'
     })
 }
 export default api;
